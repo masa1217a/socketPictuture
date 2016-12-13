5bc4c19e6c1182a80b512a882741c2591293d716
masa11217a <il.avril.song.m.i.wlcaj@gmail.com>
2016-12-13 11:04:25 +0900

    dateをintに変換
    日付と時間を分けそれぞれをintに変換

--- watchdog/src/watchdog/SetDate.java
+++ watchdog/src/watchdog/SetDate.java
@@ -7,9 +7,21 @@ import java.text.SimpleDateFormat;
 public class SetDate {
 		
 		//カレンダーを生成
-		Calendar cal = Calendar.getInstance();
-		
+		//Calendar cal = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy'_'MM'_'dd'_'HH'_'mm'_'ss");
-		
 		String str = sdf.format(new Date());
+		
+		//（今日の日付を取得）
+		Date today = Calendar.getInstance().getTime();
+		//（Dateから文字列のyyyyMMddに整形）
+		String string_Today = new SimpleDateFormat("yyyyMMdd").format(today);
+		//（文字列（yyyyMMdd）を８桁の数字に変換。）
+		Integer int_Today = Integer.parseInt(string_Today);
+		
+		//（今日の日付を取得）
+		Date time = Calendar.getInstance().getTime();
+		//（Dateから文字列のyyyyMMddに整形）
+		String string_Time = new SimpleDateFormat("HHmmss").format(time);
+		//（文字列（yyyyMMdd）を８桁の数字に変換。）
+		Integer int_Time = Integer.parseInt(string_Today);
 }
