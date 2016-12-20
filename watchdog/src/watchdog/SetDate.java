707ea1997a3a106cba46c986efffbc99a134f42f
masa11217a <il.avril.song.m.i.wlcaj@gmail.com>
2016-12-20 15:37:48 +0900

    Update SetDate.java

--- watchdog/src/watchdog/SetDate.java
+++ watchdog/src/watchdog/SetDate.java
@@ -5,23 +5,29 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class SetDate {
-		
-		//カレンダーを生成
-		//Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy'_'MM'_'dd'_'HH'_'mm'_'ss");
-		String str = sdf.format(new Date());
-		
-		//（今日の日付を取得）
-		Date today = Calendar.getInstance().getTime();
-		//（Dateから文字列のyyyyMMddに整形）
-		String string_Today = new SimpleDateFormat("yyyyMMdd").format(today);
-		//（文字列（yyyyMMdd）を８桁の数字に変換。）
-		Integer int_Today = Integer.parseInt(string_Today);
-		
-		//（今日の日付を取得）
-		Date time = Calendar.getInstance().getTime();
-		//（Dateから文字列のyyyyMMddに整形）
-		String string_Time = new SimpleDateFormat("HHmmss").format(time);
-		//（文字列（yyyyMMdd）を８桁の数字に変換。）
-		Integer int_Time = Integer.parseInt(string_Today);
+	
+	String str;
+	int Time;
+	int Today;
+	
+		public SetDate(){
+			//カレンダーを生成
+			Calendar cal = Calendar.getInstance();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy'_'MM'_'dd'_'HH'_'mm'_'ss");
+			str = sdf.format(new Date());
+			
+			//（今日の日付を取得）
+			Date today = Calendar.getInstance().getTime();
+			//（Dateから文字列のyyyyMMddに整形）
+			String string_Today = new SimpleDateFormat("yyyyMMdd").format(today);
+			//（文字列（yyyyMMdd）を８桁の数字に変換。）
+			Today = Integer.parseInt(string_Today);
+			
+			//（今日の日付を取得）
+			Date time = Calendar.getInstance().getTime();
+			//（Dateから文字列のyyyyMMddに整形）
+			String string_Time = new SimpleDateFormat("HHmmss").format(time);
+			//（文字列（yyyyMMdd）を８桁の数字に変換。）
+			Time = Integer.parseInt(string_Time);
+		}
 }
